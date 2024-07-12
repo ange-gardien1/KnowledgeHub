8e7abb3fe1ca775853f100ed1d3687ea9bb36015
Ange Gardien <angegirishyaka@gmail.com>
2024-07-12 10:45:34 +0200

    fixing build

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -3,15 +3,15 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { SES as SESClass } from "@aws-sdk/client-ses";
 import { TranscribeClient } from "@aws-sdk/client-transcribe";
 
-const { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
 
-if (!AWS_ACCESS_KEY || !AWS_SECRET_ACCESS_KEY || !AWS_REGION) {
+if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY || !AWS_REGION) {
   throw new Error("Unable to find AWS Configuration");
 }
 
 const AWS_V3_CONFIG = {
   credentials: {
-    accessKeyId: AWS_ACCESS_KEY,
+    accessKeyId: AWS_ACCESS_KEY_ID,
     secretAccessKey: AWS_SECRET_ACCESS_KEY,
     region: AWS_REGION,
   },
