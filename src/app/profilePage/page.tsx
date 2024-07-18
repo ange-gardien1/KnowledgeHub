431f5803d1aade7301cdf69f7ad66c4f84ee5010
Ange Gardien <angegirishyaka@gmail.com>
2024-07-18 14:23:07 +0200

    notification is woring

--- src/app/profilePage/page.tsx
+++ src/app/profilePage/page.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import GetNotifications from './notifications'
+
 
 
 
@@ -10,7 +10,7 @@ function ProfilePage() {
    <div>
     {/* <Sidebar/> */}
 
-   <GetNotifications/>
+<h1> hello</h1>
 
    </div>
   )
