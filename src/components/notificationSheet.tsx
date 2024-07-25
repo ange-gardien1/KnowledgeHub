559c1174ecc7f5dec4838551180594c352b7417b
Ange Gardien <angegirishyaka@gmail.com>
2024-07-25 23:28:28 +0200

    try

--- src/components/notificationSheet.tsx
+++ src/components/notificationSheet.tsx
@@ -1,59 +1,5 @@
 // components/NotificationSheet.tsx
 
-// import { trpc } from "@/app/_trpc/client";
-// import {
-//   SheetContent,
-//   SheetDescription,
-//   SheetHeader,
-//   SheetTitle,
-// } from "@/components/ui/sheet";
-// import { format } from "date-fns";
-
-// export const useNotifications = () => {
-//   return trpc.getNotificationByUserId.useQuery();
-// };
-
-// const NotificationSheet = () => {
-//   const { data, isLoading } = useNotifications();
-
-//   if (isLoading) {
-//     return <p>Loading notifications...</p>;
-//   }
-
-//   return (
-//     <SheetContent className="p-6 bg-gray-50">
-//       <SheetHeader>
-//         <SheetTitle className="text-2xl font-semibold">Notifications</SheetTitle>
-//         <SheetDescription className="text-sm text-gray-500">
-//           Here are your latest notifications.
-//         </SheetDescription>
-//       </SheetHeader>
-//       <div className="mt-4 space-y-4">
-//         {data?.map((notification) => (
-//           <div
-//             key={notification.id}
-//             className="p-4 bg-white rounded-lg shadow-sm border border-gray-200"
-//           >
-//             <p className="text-lg font-medium text-gray-800">
-//               {notification.message}
-//             </p>
-//             <p className="text-sm text-gray-500">
-//               {format(new Date(notification.createdAt), "PPpp")}
-//             </p>
-//           </div>
-//         ))}
-//       </div>
-//     </SheetContent>
-//   );
-// };
-
-// export default NotificationSheet;
-
-
-
-
-
-
 import { trpc } from "@/app/_trpc/client";
 import {
   SheetContent,
@@ -68,6 +14,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"; 
+
 export const useNotifications = () => {
   return trpc.getNotificationByUserId.useQuery();
 };
@@ -90,7 +37,7 @@ const NotificationSheet = () => {
 
   const markNotificationAsViewed = async (notificationId: string) => {
     try {
-      const result = await trpc.notification.markNotificationAsRead.useMutation();
+      await trpc.notification.markNotificationAsRead.useMutation();
       setViewNotification(notificationId);
     } catch (error) {
       console.error("Failed to update the notification", error);
